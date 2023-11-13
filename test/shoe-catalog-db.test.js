73b8bd6b055c330a489fd3448970bf4641055236
LeCodeGuy <devanj89@gmail.com>
2023-11-13 14:52:00 +0200

    added unit test template and its dependencies

---
+++ test/shoe-catalog-db.test.js
@@ -0,0 +1,22 @@
+import assert from "assert";
+import db from "../routes/database-connection.js";
+import queries from "../services/waiter-webapp-services.js";
+
+const query= queries(db);
+
+describe("Shoe Catalog API testing", function(){
+    this.timeout(6000);
+    
+    this.beforeEach(async function (){
+        await query.resetData();
+        await query.resetUsers();
+    });
+    
+    it("should return all shoe information", async function (){
+        assert.equal(true,true);
+    });
+
+    this.afterAll(function () {
+        db.$pool.end;
+    });
+})
